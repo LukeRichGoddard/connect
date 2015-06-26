6a0f6cab534605ddb997070880b784ad2b468717
Luke Richard Goddard <github@lukerichardgoddard.com>
2015-06-27 08:55:42 +1000

    Added headings to errors
    Just testing making small changes to files in git.

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
 	require_once('db.php');
 	if(!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)) {
-		echo 'Could not connect to mysql on ' . DB_HOST . '<br />';
+		echo '<h3>Error</h3>Could not connect to mysql on ' . DB_HOST . '<br />';
 		exit;
 	}
 	echo 'Connected to mysql <br />';
 	if(!mysql_select_db(DB_NAME, $dbconn)) {
-		echo 'Could not use database ' . DB_NAME . '<br />';
+		echo '<h3>Error</h3>Could not use database ' . DB_NAME . '<br />';
 		echo mysql_error() . '<br />';
 		exit;
 	}
