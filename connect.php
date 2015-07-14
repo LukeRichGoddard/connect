7dab50b70a94c954047a00c75f4d3a7c5cd288fc
Luke Richard Goddard <github@lukerichardgoddard.com>
2015-07-14 20:20:41 +1000

    Testing connect

--- connect.php
+++ connect.php
@@ -12,3 +12,25 @@
 	}
 	echo 'Connected to database ' . DB_NAME . '<br />';
 ?>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <title>Search Winestore</title>
+</head>
+<body>
+  <form action="action.php" method="POST">
+    <br />Search by winery:
+    <input type="text" name="wineryName" value="All" />
+      (type All to see all regions)
+    <br /><input type="submit" value="Search" />
+  </form>
+  <br />
+  <form action="action.php" method="POST">
+    <br />Search by region:
+    <br /><input type="submit" value="Search" />
+  </form>
+</body>
+</html>
