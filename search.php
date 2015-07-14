eb844698f04013a12e6b2c6b999db0b24590c2a4
Luke Richard Goddard <github@lukerichardgoddard.com>
2015-07-14 19:52:09 +1000

    Removed output statements for successful database connection

--- search.php
+++ search.php
@@ -1,19 +1,16 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php
 	require_once('db.php');
 	if(!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)) {
-		echo '<h3>Error</h3>Could not connect to mysql on ' . DB_HOST . '<br />';
+		echo '<h3>Error</h3><p>Could not connect to mysql on ' . DB_HOST . '</p><br />';
 		exit;
 	}
-	echo 'Connected to mysql <br />';
+	//echo 'Connected to mysql <br />';
 	if(!mysql_select_db(DB_NAME, $dbconn)) {
 		echo '<h3>Error</h3>Could not use database ' . DB_NAME . '<br />';
 		echo mysql_error() . '<br />';
 		exit;
 	}
-	echo 'Connected to database ' . DB_NAME . '<br />';
+	//echo 'Connected to database ' . DB_NAME . '<br />';
 	
 	// Build drop down menu for selecting regions
 	function buildRegionMenu($connection) {
@@ -42,6 +39,9 @@
         }
 	}
 ?>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
@@ -57,7 +57,7 @@
   <br />
   <form action="action.php" method="POST">
     <br />Search by region:
-    <?php buildRegionMenu($connection) ?>
+    <?php buildRegionMenu($dbconn) ?>
     <br /><input type="submit" value="Search" />
   </form>
 </body>
