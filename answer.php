0e4d82e74ee7edc2580cd866bcdfdf2526bd0356
Luke Richard Goddard <github@lukerichardgoddard.com>
2015-07-18 19:12:42 +1000

    Fixed missing semicolon

--- answer.php
+++ answer.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 // Only search.php should access this page
 if (basename($_SERVER[HTTP_REFERER]) != "search.php") {
     header("Location: search.php");
-    exit
+    exit;
 }
 
 require_once('db.php');
