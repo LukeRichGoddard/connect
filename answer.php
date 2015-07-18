6b2054c1fe987fe17daf712b8b49b45912b1b467
Luke Richard Goddard <github@lukerichardgoddard.com>
2015-07-19 09:04:41 +1000

    Still fixing minStock binding

--- answer.php
+++ answer.php
@@ -94,7 +94,7 @@
     // minStock
     if ($minStock != 0 or is_null($minStock)) {
         $search .= " 
-                AND SUM(inventory.on_hand) >= :minStockBind ";
+                AND on_hand >= :minStockBind ";
     }
     
     // minCost
