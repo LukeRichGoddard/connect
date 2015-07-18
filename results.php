af9f7aea225729fc3bfd3bc820e2eefe5857b648
Luke Richard Goddard <github@lukerichardgoddard.com>
2015-07-19 01:46:27 +1000

    Outputs result a table

--- results.php
+++ results.php
@@ -6,9 +6,31 @@
         parse_str($_GET["results"], $results);
         
         // Output results
-        echo '<pre>';
-        print_r($_GET["results"]);
-        echo '</pre>';
+        // echo '<pre>';
+        // print_r($_GET["results"]);
+        // echo '</pre>';
+        echo "<table>";
+        echo "<tr>";
+        echo "<td>wine_id</td>";
+        echo "<td>wine_name</td>";
+        echo "<td>wine_type</td>";
+        echo "<td>year</td>";
+        echo "<td>winery_id</td>";
+        echo "</tr>";
+        
+        // loop over results
+        foreach ($results as $result) {
+            echo "<tr>";
+            echo "<td>{$result["wine_id"]}</td>";
+            echo "<td>wine_name</td>";
+            echo "<td>wine_type</td>";
+            echo "<td>year</td>";
+            echo "<td>winery_id</td>";
+            echo "</tr>";
+        }
+        
+        echo '</table>';
+        
     } else {
         print("No results");
     }
