b116569a00237c4e2ffb1a572bd04b3537930428
Luke Richard Goddard <github@lukerichardgoddard.com>
2015-07-19 00:47:26 +1000

    Fixed $_GET

--- results.php
+++ results.php
@@ -1,10 +1,10 @@
 <?php
     require_once('common.php');
     
-    // Parse URL encoding
-    parse_str($GET["results"], $results);
-    
-    if(!is_null($results)) {
+    if(isset($_GET["results"])) {
+        // Parse URL encoding
+        parse_str($_GET["results"], $results)
+        
         // Output results
         echo '<pre>';
         print_r($results);
