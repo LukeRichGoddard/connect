c6d005a2a69c3112d0533c2db31995a85008efee
Luke Richard Goddard <github@lukerichardgoddard.com>
2015-07-18 19:10:50 +1000

    Fixed spelling error

--- answer.php
+++ answer.php
@@ -1,10 +1,9 @@
 <?php
-
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 // Only search.php should access this page
-if (basename($_SERVER[HTTP_REFERRER]) != "search.php") {
+if (basename($_SERVER[HTTP_REFERER]) != "search.php") {
     header("Location: search.php");
     exit
 }
